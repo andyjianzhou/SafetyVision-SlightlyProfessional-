87984ddc05226fce78600c2ae466afacc78fe87a
Hridyansh <dugar@ualberta.ca>
2023-01-07 15:57:33 -0700

    finished function to add images to database

--- backend/firebase-wrapper/firebase_connection.py
+++ backend/firebase-wrapper/firebase_connection.py
@@ -1,33 +1,47 @@
+from json import JSONEncoder
+import json
 import firebase_admin
 from firebase_admin import db
 import numpy as np
+
+
+class NumpyArrayEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return JSONEncoder.default(self, obj)
+
 
 class firebase_connection:
     def __init__(self):
         self.__cred_obj = firebase_admin.credentials.Certificate('safetyvision-huh.json')
         self.__default_app = firebase_admin.initialize_app(self.__cred_obj, {
-	        'databaseURL':'https://safetyvision-huh-default-rtdb.firebaseio.com/'
-	    })
+            'databaseURL': 'https://safetyvision-huh-default-rtdb.firebaseio.com/'
+        })
         self.__ref = db.reference('/')
 
-    def save_image(self,image,time=None,date=None,location=None):
+    def save_image(self, image, weapon_type=None, time=None, date=None, location=None):
         """
-        :param image: This is the image object
+        :param image: This is the image np object
+        :param weapon_type: This is the weapontype as a string
         :param time: This is the time as a string
         :param date: This is the date as a string
-        :param location: This is the location object
+        :param location: This is the string object
         :return: None
         """
 
         childref = self.__ref.child('images')
         image_json = {
-            'image':image,
-            'time':time,
-            'date':date,
-            'location':location
+            'image': image,
+            'time': time,
+            'date': date,
+            'location': location,
+            'weapon_type': weapon_type
         }
-        childref.push(image_json)
+        encoded_image_json = json.dumps(image_json, cls=NumpyArrayEncoder)
+        childref.push(encoded_image_json)
+
 
 if __name__ == '__main__':
     fc = firebase_connection()
-    fc.save_image(image='xyz')
+    fc.save_image(image=np.array([[1,2,3,4],[1,2,4,5]]))
