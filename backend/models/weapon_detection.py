5c356c1e1cb2841bf0c3673e9f018b7a42dea38c
Hridyansh <dugar@ualberta.ca>
2023-01-07 22:47:13 -0700

    Model part done

--- backend/models/weapon_detection.py
+++ backend/models/weapon_detection.py
@@ -23,38 +23,60 @@ def plot_bounding_boxes(frame, labels, bounding_boxes):
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
         cv2.putText(frame, labels[label], (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
     return frame
-    
-fc = firebase_connection()
-labels = {0: "Knife", 1:"Gun"}
-model = torch.hub.load("yolov5", 'custom', path="yolov5s.pt", source='local') 
-model.conf = 0.6
-# model.conf = 0.05
-model.eval()
 
-# image = cv2.imread("gun-test.jpg")
+ct = None
+def detect():
+    global ct
+    fc = firebase_connection()
+    labels = {0: "Knife", 1:"Gun"}
+    model = torch.hub.load("yolov5", 'custom', path="yolov5s.pt", source='local')
+    model.conf = 0.65
+    # model.conf = 0.05
+    model.eval()
 
-#use webcam
-print("Starting webcam...")
-cap = cv2.VideoCapture(0)
+    # image = cv2.imread("gun-test.jpg")
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        print("failed to grab frame")
-        break
-    #convert frame to PIL image
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    output = model(frame)
-    output.render()
+    #use webcam
+    print("Starting webcam...")
+    cap = cv2.VideoCapture(0)
 
-    for img in output.render():
-        img = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-        cv2.imshow("frame", img)
-        if cv2.waitKey(1) == ord("q"):
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("failed to grab frame")
             break
+        #convert frame to PIL image
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        output = model(frame)
+        output.render()
+
+        for img in output.render():
+            img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+            cv2.imshow("frame", img)
+            if 'pistol' in str(output):
+                if ct is None:
+                    print('found weapon')
+                    ct = time.time()
+                    current_day = date.today()
+                    current_time = datetime.now().strftime("%H:%M:%S")
+                    weapon = str(output)
+                    weapon = 'pistol' if 'pistol' in weapon else 'knife'
+                    print(weapon)
+                    # boxes = output.xyxy[0][:, :-1].numpy() # get the bounding boxes
+                    # convert output to numpy array
+                    image = np.array(img) # this is the image with bounding boxes and labels
+                    fc.save_image(image=image, weapon_type=weapon,location='DICE', time=str(current_time), date=str(current_day))
+                else:
+                    if time.time() - ct > 300:
+                        ct = None
+            if cv2.waitKey(1) == ord("q"):
+                 break
 
 
-    
+def main():
+    while True:
+        detect()
+main()
 # use webcam and plot bounding boxes on screen through opencv
 
 
