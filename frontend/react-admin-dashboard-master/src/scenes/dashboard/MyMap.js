2d7f3896fee463227c58933959cf4697e20e0b02
Manav Panchal <manav03panchal@gmail.com>
2023-01-08 05:58:31 -0700

    Fixed

--- frontend/react-admin-dashboard-master/src/scenes/dashboard/MyMap.js
+++ frontend/react-admin-dashboard-master/src/scenes/dashboard/MyMap.js
@@ -1,17 +1,32 @@
 import { useState } from "react";
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
 import React from "react";
+
 import mapStyles from "./mapStyles";
 const mapContainerStyle = {
   width: "800px",
   height: "600px",
 };
-
+const SafeImage =
+  "https://firebasestorage.googleapis.com/v0/b/safetyvision-huh.appspot.com/o/safe_icon.png?alt=media&token=df245587-62dd-48a1-8ce6-6a4889633ba7";
+const UnsafeImage =
+  "https://firebasestorage.googleapis.com/v0/b/safetyvision-huh.appspot.com/o/icons8-error-30.png?alt=media&token=c36080a1-b75d-4389-8ec7-12317620feb5";
 const center = {
   lat: 53.5264962,
   lng: -113.5257753,
 };
-
+const markers = [
+  {
+    position: { lat: 53.5264962, lng: -113.5257753 },
+    name: "Main Quad",
+    status: "Not Safe",
+  },
+  {
+    position: { lat: 53.5282209, lng: -113.5296782 },
+    name: "DICE Building",
+    status: "Safe",
+  },
+];
 function MyMap() {
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
@@ -21,7 +36,6 @@ function MyMap() {
   const [map, setMap] = useState(null);
 
   const onLoad = React.useCallback(function callback(map) {
-    // This is just an example of getting and using the map instance!!! don't just blindly copy!
     const bounds = new window.google.maps.LatLngBounds(center);
     map.fitBounds(bounds);
 
@@ -35,12 +49,22 @@ function MyMap() {
   return isLoaded ? (
     <GoogleMap
       mapContainerStyle={mapContainerStyle}
-      options={{ styles: mapStyles, minZoom: 4, maxZoom: 14 }}
+      options={{ styles: mapStyles, minZoom: 4, maxZoom: 16 }}
       defaultCenter={{ lat: 53.5264962, lng: -113.5257753 }}
       center={center}
       onLoad={onLoad}
       onUnmount={onUnmount}
     >
+      {markers.map((marker, index) => (
+        <Marker
+          key={index}
+          position={marker.position}
+          icon={{
+            url: marker.status === "Safe" ? SafeImage : UnsafeImage,
+          }}
+        />
+      ))}
+
       {/* Child components, such as markers, info windows, etc. */}
       <></>
     </GoogleMap>
