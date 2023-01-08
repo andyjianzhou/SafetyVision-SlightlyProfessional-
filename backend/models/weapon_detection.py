c66e943d0de89ac863a79c0d0150979d488e54a1
PandaOnTheFly <74941296+andyjianzhou@users.noreply.github.com>
2023-01-07 19:40:20 -0700

    Fixed model
    Successfully added into firebase

--- backend/models/weapon_detection.py
+++ backend/models/weapon_detection.py
@@ -3,16 +3,12 @@ import torch
 from io import BytesIO
 import numpy as np
 from PIL import Image
-import os
-import sys 
-
-sys.path.insert(0, 'SafetyVision-huh-/backend/firebase_wrapper')
-
 from firebase_connection import firebase_connection
-
+from datetime import date, datetime
+import time
 # Initialize the FirebaseWrapper class
 fc = firebase_connection()
-
+labels = {0: "Knife", 1:"Gun"}
 model = torch.hub.load("yolov5", 'custom', path="yolov5s.pt", source='local') 
 model.conf = 0.30
 # model.conf = 0.05
@@ -21,11 +17,21 @@ model.eval()
 
 image = cv2.imread("gun-test.jpg")
 output = model(image)
+#get the label NAME of what is detected
 output.show()
-print(output)
+
 if not output:
   print("No weapons detected")
 else:
-  # convert output to numpy array
-  image = np.array(output.render()) # this is the image with bounding boxes and labels
-  #convert to cv2
+    current_day = date.today()
+    current_time = datetime.now().strftime("%H:%M:%S")
+    weapon = str(output)
+    weapon = 'pistol' if 'pistol' in weapon else 'knife'
+    print(weapon)
+    # boxes = output.xyxy[0][:, :-1].numpy() # get the bounding boxes
+    # convert output to numpy array
+    image = np.array(output.render()) # this is the image with bounding boxes and labels
+    fc.save_image(image=image, weapon_type=weapon,location='DICE', time=str(current_time), date=str(current_day))
+    #sleep for 10 minutes
+    time.sleep(600)
+    #convert to cv2
